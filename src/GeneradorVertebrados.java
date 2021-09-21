de3e504bf0efb277f7425a36d77be3f722a8b723
fekicode <marino.adrian@gmail.com>
2021-09-21 08:09:48 -0500

    Cambio de tiburón y paloma por oviparo

--- src/GeneradorVertebrados.java
+++ src/GeneradorVertebrados.java
@@ -3,8 +3,9 @@ public class GeneradorVertebrados {
         Vertebrado[] miVertebrados = new Vertebrado[4];
         miVertebrados[0] = new Vertebrado("Camello", "Mamifero");
         miVertebrados[1] = new Vertebrado("Vaca", "Mamifero");
-        miVertebrados[2] = new Vertebrado("Tiburón", "Pez");
-
+        miVertebrados[2] = new Vertebrado("Tiburón", "Oviparo");
+        miVertebrados[3] = new Vertebrado("Paloma", "Oviparo");
+        
         return miVertebrados;
     }
 }
